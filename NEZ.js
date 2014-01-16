a41a6ef03b1fdaf3e4f86f35818d6e50084b5501
wxttt <wangxiaottt@gmail.com>
2014-01-17 01:38:17 +0800

    step 1 improvement

--- NEZ.js
+++ NEZ.js
@@ -1,32 +1,29 @@
-Users = new Meteor.Collection("users");
+Coders = new Meteor.Collection("coders");
 Events = new Meteor.Collection("events")
 
 if (Meteor.isClient) {
     Template.zheng.users = function(){
-        return Users.find({});
+        return Coders.find({});
     }
 
     Template.zheng.events = function(){
-        return Events.find({}, {sort: {time: -1}});
+        return Events.find({}, {sort: {time: -1}, limie: 5});
     }
 
-    Template.user.selected = function () {
-        return Session.equals("selected_player", this._id) ? "selected" : '';
-    };
 
-    Template.user.events({
+    Template.coder.events({
         'click .add': function (_event) {
-            Users.update({_id:this._id}, {$inc: {score: 1}})
+            Coders.update({_id:this._id}, {$inc: {score: 1}})
             Events.insert({name:this.name, event: '被狠狠的加了一笔!', time: new Date().getTime()});
         },
         'click .minus': function(_event){
             console.log('min');
-            Users.update({_id:this._id}, {$inc: {score: -1}})
+            Coders.update({_id:this._id}, {$inc: {score: -1}})
             Events.insert({name:this.name, event: '贱贱的减去了一笔!', time: new Date().getTime()});
         },
         'click .bg': function(_event){
             if(this.score >= 5){
-                Users.update({_id:this._id}, {$inc: {score: -5}})
+                Coders.update({_id:this._id}, {$inc: {score: -5}})
                 Events.insert({name:this.name, event: '慷慨的bg了大家！', time: new Date().getTime()});
             }else{
                 alert("尚未凑齐5笔，无法召唤bg大神！")
@@ -38,7 +35,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
     Meteor.startup(function () {
         // code to run on server at startup
-        if (Users.find().count() === 0) {
+        if (Coders.find().count() === 0) {
             var names = ["林大灰狼",
                 "老姜",
                 "李伟",
@@ -48,7 +45,7 @@ if (Meteor.isServer) {
                 "詹副帅",
                 "王筱"];
             for (var i = 0; i < names.length; i++)
-                Users.insert({name: names[i], score: 0});
+                Coders.insert({name: names[i], score: 0});
         }
     });
 }
